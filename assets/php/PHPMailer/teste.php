926b5412da943aa07624c2c27efdae00e51133cc
Eleu Carlos <user.name>
2020-05-06 11:54:01 -0300

    update contato

--- assets/php/PHPMailer/teste.php
+++ assets/php/PHPMailer/teste.php
@@ -1,8 +1,8 @@
 <?php
 require 'PHPMailerAutoload.php';
 $mail = new PHPMailer;
-$mail->setFrom('from@example.com', 'Ricardo Almeida');
-$mail->addAddress('kdim_df@yahoo.com.br', 'My Friend');
+$mail->setFrom('contato@liderarti.com.br', 'Formulário Site');
+$mail->addAddress('eleu@liderarti.com.br', 'Contato');
 $mail->Subject  = 'First PHPMailer Message';
 $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
 if(!$mail->send()) {
